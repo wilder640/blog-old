256adff6b5afed26786fdbca5012c1349c6332bf
wilder640 <wei922565@gmail.com>
2024-11-21 09:54:15 +0800

    增加js method為get

--- docs/assets/javascripts/stats.js
+++ docs/assets/javascripts/stats.js
@@ -1,6 +1,7 @@
 document$.subscribe(function() {
     fetch('https://api.umami.is/v1/websites/53197f63-8bcf-4ee7-b1dc-3bb98354a997/stats?startAt=0&endAt=' + Date.now(), {
-      headers: {
+        method: 'Get',
+        headers: {
         'Content-Type': 'application/json',
         'x-umami-api-key': 'oGTvKvSH1fYV2wHXX3kEbkSEAHwnc6Tn'
       }
@@ -9,6 +10,5 @@ document$.subscribe(function() {
     .then(data => {
       document.getElementById('visits-count').textContent = data.visits.value;
     });
+    console.log(data.visits.value);
   });
-
-  console.log(data.visits.value);
